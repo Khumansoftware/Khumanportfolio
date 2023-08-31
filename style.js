9db2eddac429f1726a93d203387a26f87faef0fe
Khumansoftware <143687799+Khumansoftware@users.noreply.github.com>
2023-08-30 23:39:38 -0700

    first commit

---
+++ style.js
@@ -0,0 +1,9 @@
+function infom() {
+    var temp = confirm("Click OK [You Want Gmail] or Click Cancel [You Want Number]");
+    if (temp == true) {
+        alert("Email= skhuman2003@gmail.com ");
+
+    } else {
+        alert("Phone Number = 7737817383");
+    }
+}
